929c48bd68634f3e59afa2fd8ca298071e375a2b
Jeffrey <Jeffrey@simpleyapp.com>
2020-05-04 12:53:25 -0400

    Initial Commit

---
+++ Swift Hangman/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift Hangman
+//
+//  Created by Jeffrey  on 5/4/20.
+//  Copyright © 2020 BMCC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
